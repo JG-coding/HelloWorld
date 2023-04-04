d0c2b3bac3e32fe2126ac4374c5c2ceaf03c3103
JG-coding <1149878085@qq.com>
2023-04-04 10:08:39 +0800

    创建main.cpp文件，写了初始代码

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#incude <iostream>
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+
+int main()
+{
+    int arr[] = {12,55,66,77,1,3};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr,size);
+    
+    return 0;
+}
